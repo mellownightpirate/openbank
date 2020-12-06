6018c7eda2803895f3b34472838b2820c8d95018
mellownightpirate <a@hasan.co>
2020-12-06 19:46:01 +0000

    ready to deploy

---
+++ routes/api/plaid.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const plaid = require("plaid");
+const router = express.Router();
+const passport = require("passport");
+const moment = require("moment");
+const mongoose = require("mongoose");
+
+// Load Account and User models
+const Account = require("../../models/Account");
+const User = require("../../models/User");
+
+const PLAID_CLIENT_ID = "5f05e314f330e200121f05eb";
+const PLAID_SECRET = "5b7180c865229640ed11b31243f6a7";
+const PLAID_PUBLIC_KEY = "d216519a084a5b106b08f58d01fcba";
+
+const client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments.sandbox,
+  { version: "2018-05-22" }
+);
+
+var PUBLIC_TOKEN = null;
+var ACCESS_TOKEN = null;
+var ITEM_ID = null;
+
+// Routes will go here
+// GET  api/plaid/accounts
+// Get all accounts linked with plaid for a specific user
+// Private
+router.get(
+  "/accounts",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Account.find({ userId: req.user.id })
+      .then(accounts => res.json(accounts))
+      .catch(err => console.log(err));
+  }
+);
+
+// POST api/plaid/accounts/add
+// Trades public token for access token and stores credentials in database
+// Private
+router.post(
+  "/accounts/add",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    PUBLIC_TOKEN = req.body.public_token;
+const userId = req.user.id;
+const institution = req.body.metadata.institution;
+    const { name, institution_id } = institution;
+if (PUBLIC_TOKEN) {
+      client
+        .exchangePublicToken(PUBLIC_TOKEN)
+        .then(exchangeResponse => {
+          ACCESS_TOKEN = exchangeResponse.access_token;
+          ITEM_ID = exchangeResponse.item_id;
+
+// Check if account already exists for specific user
+Account.findOne({
+  userId: req.user.id,
+  institutionId: institution_id
+})
+  .then(account => {
+    if (account) {
+      console.log("Account already exists");
+    } else {
+      const newAccount = new Account({
+        userId: userId,
+        accessToken: ACCESS_TOKEN,
+        itemId: ITEM_ID,
+        institutionId: institution_id,
+        institutionName: name
+      });
+newAccount.save().then(account => res.json(account));
+    }
+  })
+  .catch(err => console.log(err)); // Mongo Error
+})
+.catch(err => console.log(err)); // Plaid Error
+}
+}
+);     
+
+// DELETE api/plaid/accounts/:id
+// Delete account with given id
+// Private
+router.delete(
+  "/accounts/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Account.findById(req.params.id).then(account => {
+      // Delete account
+      account.remove().then(() => res.json({ success: true }));
+    });
+  }
+)
+
+// POST api/plaid/accounts/transactions
+// Fetch transactions from past 30 days from all linked accounts
+// Private
+router.post(
+  "/accounts/transactions",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const now = moment();
+    const today = now.format("YYYY-MM-DD");
+    const thirtyDaysAgo = now.subtract(30, "days").format("YYYY-MM-DD"); // Change this if you want more transactions
+let transactions = [];
+const accounts = req.body;
+if (accounts) {
+      accounts.forEach(function(account) {
+        ACCESS_TOKEN = account.accessToken;
+        const institutionName = account.institutionName;
+client
+          .getTransactions(ACCESS_TOKEN, thirtyDaysAgo, today)
+          .then(response => {
+            transactions.push({
+              accountName: institutionName,
+              transactions: response.transactions
+            });
+// Don't send back response till all transactions have been added
+if (transactions.length === accounts.length) {
+              res.json(transactions);
+            }
+          })
+          .catch(err => console.log(err));
+      });
+    }
+  }
+);
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,107 @@
+// pull in dependencies and input validations and user models
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+// Load User model
+const User = require("../../models/User");
+// POST api/users/register
+router.post("/register", (req, res) => {
+  // Form validation
+  // Pull the errors and isValid variables from our validateRegisterInput(req.body) function
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  // If valid input, use MongoDB's User.findOne() to see if the user already exists
+  User.findOne({ email: req.body.email }).then((user) => {
+    // If user is a new user, fill in the fields (name, email, password) with data sent in the body of the request
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      // Hash password before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// POST api/users/login
+// Login user and return JWT token
+router.post("/login", (req, res) => {
+  // Form validation
+  // Pull the erros and isValid variabls from validateLoginInput(req.body) function and check input validation
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then((user) => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "Email not found" });
+    }
+
+    // Use bcryptjs to compare submitted password with hashed password in our database
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+        };
+
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926, // 1 year in seconds
+          },
+            (err, token) => {
+            //   If successful, append the token to a Bearer string
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
+
+module.exports = router;
