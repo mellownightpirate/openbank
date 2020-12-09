ec4efc79b687e50f798ffba7fc9a8b6d3354ef84
mellownightpirate <a@hasan.co>
2020-12-09 19:44:02 +0000

    removed node_modules

---
+++ client/build/precache-manifest.79bbcc1576b6712da793c4bd9b3df26c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f5db3ab07ddae4cb02",
+    "url": "/static/css/main.395b2530.chunk.css"
+  },
+  {
+    "revision": "e6f5db3ab07ddae4cb02",
+    "url": "/static/js/main.e6f5db3a.chunk.js"
+  },
+  {
+    "revision": "59d51f83909f64a4816e",
+    "url": "/static/js/1.59d51f83.chunk.js"
+  },
+  {
+    "revision": "92040c44687fa46f37de",
+    "url": "/static/js/2.92040c44.chunk.js"
+  },
+  {
+    "revision": "e6ccfb90997761ec2a62",
+    "url": "/static/js/3.e6ccfb90.chunk.js"
+  },
+  {
+    "revision": "55e77a4fa16964a2c124",
+    "url": "/static/js/4.55e77a4f.chunk.js"
+  },
+  {
+    "revision": "8660610d038fea55d858",
+    "url": "/static/js/runtime~main.8660610d.js"
+  },
+  {
+    "revision": "d6ae32176b92c2fd029ed75b9801e8ce",
+    "url": "/static/media/credit_card.d6ae3217.png"
+  },
+  {
+    "revision": "0cf15d39bbcc3e07d1e613576fad3483",
+    "url": "/index.html"
+  }
+];
